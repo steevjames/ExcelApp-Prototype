97f9088056a004cc6d9081694afc007e6e83ee0c
Joyal A Johney <joyalajohny@gmail.com>
2020-03-20 23:02:15 +0530

    app initialized

---
+++ android/app/src/main/kotlin/com/example/excelapp_prototype/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.excelapp_prototype
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
